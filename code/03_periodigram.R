48b34f8ba4a488548547b09f10e28a2a10846b2b
Alison <you@example.com>
2025-09-23 21:35:27 -0600

    add subsetting lines

--- code/03_periodigram.R
+++ code/03_periodigram.R
@@ -20,6 +20,8 @@ df <- subset(df, verticalPosition %in% ml)
 #in lsp, x must be only 2 columns (time, data)
 df <- df[,c("timeBgn", "data.isoH2o.dlta18OH2o.mean")]
 
+
+## STOP here and continue on line 42 if subsetting dates
 #convert timeBgn to numeric
 df$timeBgn <- ifelse(nchar(df$timeBgn) == 10,       # length of "YYYY-MM-DD"
                            paste0(df$timeBgn, " 00:00:00"), # append midnight
@@ -36,3 +38,41 @@ lsp(df,
     alpha = 0.05, 
     plot = T)
 
+
+## START here
+# test lsp with a smaller dataset: 1 year
+df <- subset(df, timeBgn >= as.POSIXct("2022-01-01 00:00", tz="GMT") & 
+                           timeBgn <= as.POSIXct("2022-12-31 23:59", tz="GMT"))
+
+df$timeBgn <- ifelse(nchar(df$timeBgn) == 10,       # length of "YYYY-MM-DD"
+                     paste0(df$timeBgn, " 00:00:00"), # append midnight
+                     df$timeBgn)
+df$timeBgn <- as.POSIXct(df$timeBgn, format="%Y-%m-%d %H:%M:%S", tz="GMT")
+df$timeBgn <- as.numeric(df$timeBgn)
+
+lsp(df, 
+    from = 1/(10*24), #starting (lowest) frequency ( = 1 cycle per 10d (converted to hours))
+    to = 1,  #highest frequency to analyze (1 = 1 cycle per hour)
+    type = "frequency", 
+    ofac = 2, #oversampling factor (usually 2-4)
+    alpha = 0.05, 
+    plot = T)
+
+
+# test lsp with a smaller dataset: 4 months
+df <- subset(df, timeBgn >= as.POSIXct("2022-01-01 00:00", tz="GMT") & 
+               timeBgn <= as.POSIXct("2022-04-30 23:59", tz="GMT"))
+
+df$timeBgn <- ifelse(nchar(df$timeBgn) == 10,       # length of "YYYY-MM-DD"
+                     paste0(df$timeBgn, " 00:00:00"), # append midnight
+                     df$timeBgn)
+df$timeBgn <- as.POSIXct(df$timeBgn, format="%Y-%m-%d %H:%M:%S", tz="GMT")
+df$timeBgn <- as.numeric(df$timeBgn)
+
+lsp(df, 
+    from = 1/(10*24), #starting (lowest) frequency ( = 1 cycle per 10d (converted to hours))
+    to = 1,  #highest frequency to analyze (1 = 1 cycle per hour)
+    type = "frequency", 
+    ofac = 2, #oversampling factor (usually 2-4)
+    alpha = 0.05, 
+    plot = T)
