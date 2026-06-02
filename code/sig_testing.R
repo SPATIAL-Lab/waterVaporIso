c7312ec59311e0f040674d456a2aeb0b1b0c4fb8
Alison <alisondernbach@gmail.com>
2026-06-02 16:30:41 -0600

    mostly redfit stuff

--- code/sig_testing.R
+++ code/sig_testing.R
@@ -8,24 +8,58 @@
 #install.packages("dplR")
 library(dplR)
 
+site <- "HARV"
+ml <- "top"        #10 or "top"
+
+# load data from fitting_01_NEONdata
+df <- read.csv(paste0("data/output/fitting_NEONdata_results_", site, "_", ml, ".csv"))
+
+df$timeBgn <- ifelse(nchar(df$timeBgn) == 10,       # length of "YYYY-MM-DD"
+                     paste0(df$timeBgn, " 00:00:00"), # append midnight
+                     df$timeBgn)
+df$timeBgn <- as.POSIXct(df$timeBgn, format="%Y-%m-%d %H:%M:%S", tz="GMT")
+
+# get times and values for function
+times <- df$elapsed_days 
+values <- df$residuals_phi
+
+#segment data
+window <- 1200 #days
+
+df$elapsed_days[which(df$timeBgn == "2020-01-01 00:00:00")] #to get idx of a specific date
+idx <-  1037  #window start elapsed day
+
+segment_times <- times[which(times == idx):which(times == (idx + window))] #get times and values for that segment
+segment_values <- values[which(times ==idx):which(times == (idx + window))] #get times and values for that segment
+
+plot(segment_times, segment_values, 
+     #type = "l", 
+     cex = 0.8, pch = 19, 
+     main = paste("Segment starting", df$timeBgn[which(times == idx)]), 
+     xlab = "Elapsed Days", ylab = "iso_pre-zeroed")
+
+
+
 
 # 
-redf.dat <- redfit(x = segment_values, t = segment_times, nsim = 100)
+redf.dat <- redfit(x = segment_values, t = segment_times, 
+                   n50 = floor((max(segment_times)-min(segment_times))/20), #this will be close to 20-day windows, but with 50% overlap...
+                   nsim = 100)
 
 par(tcl = 0.5, mar = rep(2.2, 4), mgp = c(1.1, 0.1, 0),xaxs="i")
 
 
-plot(1/redf.dat[["freq"]][7:100], redf.dat[["gxxc"]][7:100],
+plot(1/redf.dat[["freq"]][7:200], redf.dat[["gxxc"]][7:200], #7:100 ensures only periods between ~20 and ~1 are included
      ylim = range(redf.dat[["ci99"]]/2, redf.dat[["gxxc"]]),
      type = "n", ylab = "Spectrum", xlab = "Period (days)",
-     main = "testing redfit() on a 60-day segment of data",
+     main = paste0(window, "-day segment starting ", df$timeBgn[which(times == idx)]),
      axes = FALSE)
 grid()
-lines(1/redf.dat[["freq"]][7:100], redf.dat[["gxxc"]][7:100], col = "black",lwd=1.5)
-lines(1/redf.dat[["freq"]][7:100], smooth.spline(redf.dat[["ci99"]][7:100],spar = 0.8)$y, col = "#D95F02")
-lines(1/redf.dat[["freq"]][7:100], smooth.spline(redf.dat[["ci95"]][7:100],spar = 0.8)$y, col = "#7570B3")
-lines(1/redf.dat[["freq"]][7:100], smooth.spline(redf.dat[["ci90"]][7:100],spar = 0.8)$y, col = "#E7298A")
-freqs <- pretty(1/redf.dat[["freq"]][7:100])
+lines(1/redf.dat[["freq"]][7:200], redf.dat[["gxxc"]][7:200], col = "black",lwd=1.5)
+lines(1/redf.dat[["freq"]][7:200], smooth.spline(redf.dat[["ci99"]][7:200],spar = 0.8)$y, col = "#D95F02")
+lines(1/redf.dat[["freq"]][7:200], smooth.spline(redf.dat[["ci95"]][7:200],spar = 0.8)$y, col = "#7570B3")
+lines(1/redf.dat[["freq"]][7:200], smooth.spline(redf.dat[["ci90"]][7:200],spar = 0.8)$y, col = "#E7298A")
+freqs <- pretty(1/redf.dat[["freq"]][7:200])
 pers <- round(1 / freqs, 2)
 axis(1, at = freqs, labels = TRUE)
 axis(2)
